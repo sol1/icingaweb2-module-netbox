d227345bd989a957a79bf0753764e644c56263d2
matt <matthew.smith@sol1.com.au>
2022-02-21 14:47:54 +1100

    opps

--- library/Netbox/Netbox.php
+++ library/Netbox/Netbox.php
@@ -204,13 +204,13 @@ class Netbox
 					// Default empty values for column headings
 					foreach ($satellite_keys as $s) {
 						$row->{'icinga_satellite_' . $s} = NULL;
-					};
-					foreach ($host_keys, $h)) {
+					}
+					foreach ($host_keys as $h)) {
 						$row->{'icinga_host_' . $h} = NULL;
-					};
-					foreach ($other_keys, $o)) {
+					}
+					foreach ($other_keys as $o)) {
 						$row->{'icinga_' . $o} = NULL;
-					};
+					}
 
 					// Parse the data and set values
 					$icinga = $row->config_context->icinga;
