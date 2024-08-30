9490001751ec5b61e91696fd26f8cd2b881540c2
Matthew Smith <matthew.smith@sol1.com.au>
2024-08-30 12:38:20 +1000

    boop

--- library/Netbox/ProvidedHook/Director/ImportSource.php
+++ library/Netbox/ProvidedHook/Director/ImportSource.php
@@ -194,12 +194,12 @@ class ImportSource extends ImportSourceHook
 					if (!isset($v['custom_fields']->icinga_monitored) || $v['custom_fields']->icinga_monitored === true) {
 						
 						// if we have a icinga_var_type then make a list of type to make
-						$var_types = ['default'];
+						$var_types = ['all'];
 						if (isset($v['custom_fields']->icinga_var_type)) {
 							if (is_string($v['custom_fields']->icinga_var_type) && !$v['custom_fields']->icinga_var_type == '') {
-								$var_types = explode(',', $v['custom_fields']->icinga_var_type);
+								$var_types = array_merge($var_types, explode(',', $v['custom_fields']->icinga_var_type));
 							} elseif (is_array($v['custom_fields']->icinga_var_type) and !empty($v['custom_fields']->icinga_var_type)) {
-								$var_types = $v['custom_fields']->icinga_var_type;
+								$var_types = array_merge($var_types, $v['custom_fields']->icinga_var_type);
 							} else {
 								die;
 							}
