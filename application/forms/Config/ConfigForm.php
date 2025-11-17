18d84446b21e5203953cb117acc699dfe2e8bc5c
Steffen Brandemann <steffen@brandemann.net>
2025-11-17 22:26:47 +0100

    global config fixes

--- application/forms/Config/ConfigForm.php
+++ application/forms/Config/ConfigForm.php
@@ -15,27 +15,30 @@ class ConfigForm extends BaseConfigForm
 
     public function createElements(array $formData)
     {
-        $this->addElement('text', 'baseurl', [
+        $this->addElement('text', 'netbox_baseurl', [
             'label'       => $this->translate('NetBox API URL'),
             'description' => $this->translate('Base URL to the Netbox API, e.g. https://netbox.example.com/api'),
+            'placeholder' => 'https://netbox.example.com/api',
+            'required'    => true,
         ]);
 
-        $this->addElement('password', 'apitoken', [
+        $this->addElement('password', 'netbox_apitoken', [
             'label'       => $this->translate('API Token'),
             'description' => $this->translate('See https://netbox.example.com/user/api-tokens'),
+            'required'    => true,
         ]);
 
-        $this->addElement('text', 'proxy', [
+        $this->addElement('text', 'netbox_proxy', [
             'label'       => $this->translate('Proxy'),
             'description' => $this->translate('Optional proxy server setting in the format <address>:<port>'),
         ]);
 
-        $this->addElement('checkbox', 'ssl_enable', [
+        $this->addElement('checkbox', 'netbox_ssl', [
             'label' => $this->translate('Enable SSL checks'),
             'description' => $this->translate('Checking this box will cause the Netbox import module enable SSL certificate verification and SSL hostname check.'),
         ]);
 
-        $this->addElement('checkbox', 'slug_keyids', [
+        $this->addElement('checkbox', 'netbox_slug', [
             'label'       => $this->translate('Slug keyid\'s'),
             'description' => $this->translate('Where available use slugs for keyid\'s.'),
         ]);
